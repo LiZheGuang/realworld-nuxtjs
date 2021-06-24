b0f3254f0432b701468d78e12c6386cb0505e467
zheguang <zheguang@guanghe.tv>
2021-06-24 19:03:06 +0800

    feat: 🎸 案列demo

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -6,7 +6,7 @@
 
         <h1>{{ article.title }}</h1>
 
-        <!-- <article-meta :article="article" /> -->
+        <article-meta :article="article" />
 
       </div>
     </div>
@@ -20,14 +20,14 @@
       <hr />
 
       <div class="article-actions">
-        <!-- <article-meta :article="article" /> -->
+        <article-meta :article="article" />
       </div>
 
       <div class="row">
 
         <div class="col-xs-12 col-md-8 offset-md-2">
 
-          <!-- <article-comments :article="article" /> -->
+          <article-comments :article="article" />
 
         </div>
 
@@ -39,20 +39,37 @@
 </template>
 
 <script>
-import { getArticle } from "@/api/article";
+import { getArticle } from '@/api/article'
+import MarkdownIt from 'markdown-it'
+import ArticleMeta from './components/article-meta'
+import ArticleComments from './components/article-comments'
 
 export default {
-  name: "ArticleIndex",
-  async asyncData({ params }) {
-    const { data } = await getArticle(params.slug);
-    const { article } = data;
-    // const md = new MarkdownIt();
-    // article.body = md.render(article.body);
+  name: 'ArticleIndex',
+  async asyncData ({ params }) {
+    const { data } = await getArticle(params.slug)
+    const { article } = data
+    const md = new MarkdownIt()
+    article.body = md.render(article.body)
     return {
-      article,
-    };
+      article
+    }
   },
-};
+  components: {
+    ArticleMeta,
+    ArticleComments
+  },
+  head () {
+    return {
+      title: `${this.article.title} - RealWorld`,
+      meta: [
+        { hid: 'description', name: 'description', content: this.article.description }
+      ]
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
